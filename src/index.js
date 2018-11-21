191eb011a4314f8ac9179c91c74205eaf1c381b7
Pj Ofodile <pjfides@gmail.com>
2018-11-21 21:09:38 +0100

    index.js edit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
 import style from "./_scss/main.scss";
 
-console.log(`I'm a silly entry point`);
-
-const arr = [1, 2, 3];
-const iAmJavascriptES6 = () => console.log(...arr);
-window.iAmJavascriptES6 = iAmJavascriptES6;
+console.log(`Hello we're live!`);
